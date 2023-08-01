2e67a38d84e23c37af1f7308e95e459a7e32bfa7
Koomaxx86 <bluejjw86@daum.net>
2023-08-01 21:44:21 +0900

    자바 미션 2 완성

--- JAVA MISSION/src/_02.java
+++ JAVA MISSION/src/_02.java
@@ -5,15 +5,37 @@ public class _02 {
 
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
-		int num1; // 홀수의 합계
-		int num2; // 짝수의 합계
+		int sum1 = 0; // 홀수의 합계
+		int sum2 = 0; // 짝수의 합계
 
+		// 홀수 출력
 		for (int i = 1; i <= input; i++) {
-			if (i % 2 == 1 && != i) {
+			if (i % 2 == 1 && i != input-1 && i != input) {
 				System.out.print(i + "+");
-				if (i % 2 == 1 && i == input - 1) {
-					System.out.print(i);
-				}
+				sum1 += i;
+			} else if (i % 2 == 1) {
+				System.out.print(i);
+				sum1 += i;
+			}
+		}
+		System.out.println("=" + sum1);
+
+		// 짝수 출력
+		for (int i = 1; i <= input; i++) {
+			if (i % 2 == 0 && i != input-1 && i != input) {
+				System.out.print(i + "+");
+				sum2 += i;
+			} else if (i % 2 == 0) {
+				System.out.print(i);
+				sum2 += i;
+			}
+		}
+		System.out.println("=" + sum2);
+
+		// 약수 출력
+		for (int i = 1; i <= input; i++) {
+			if (input % i == 0) {
+				System.out.print(i + " ");
 			}
 		}
 	}
