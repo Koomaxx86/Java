7d83f8ba23860e8cf81c35c7d406af4546fef107
Koomaxx86 <bluejjw86@daum.net>
2023-08-18 18:17:59 +0900

    8/18

---
+++ JavaFX_ListView/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			// css를 불러와서 scene에 종속
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// scene를 stage에 올린다
+			stage.setScene(scene);
+			// stage를 출력한다.
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
