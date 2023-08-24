2f65f89b63371d5f7e13a991937c317c98182a59
Koomaxx86 <bluejjw86@daum.net>
2023-08-24 09:07:59 +0900

    8/24 오전 학원

--- JavaFX_TableView/src/application/Main.java
+++ JavaFX_TableView/src/application/Main.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
+		
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root);
