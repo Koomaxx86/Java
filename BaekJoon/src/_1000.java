6d2c3ded59315c37294ef32949cee22fb70dadbe
Koomaxx86 <bluejjw86@daum.net>
2024-01-12 12:13:54 +0900

    0112 1213

---
+++ BaekJoon/src/_1000.java
@@ -0,0 +1,16 @@
+import 
+
+public class _1000 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+
+        if( a > 0 && b < 10) {
+            int sum = a+b;
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
